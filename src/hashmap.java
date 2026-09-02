04104341aa78f5a0422f92099e1cd3f6f1a3b8e5
Parveen <parveen.mansuraligan@gmail.com>
2026-09-02 21:33:00 +0530

    Added hashmap program

---
+++ src/hashmap.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class hashmap {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(101, "ravi");
+        map.put(102, "anu");
+        System.out.println(map);
+    }
+
+}
