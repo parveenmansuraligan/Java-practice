09cccadc1ba44842ff4ef46e6d9458a08b2f2811
Parveen <parveen.mansuraligan@gmail.com>
2026-09-04 07:05:57 +0530

    Updated Hashmap with all methods

--- src/hashmap.java
+++ src/hashmap.java
@@ -4,7 +4,20 @@ public class hashmap {
         HashMap<Integer, String> map = new HashMap<>();
         map.put(101, "ravi");
         map.put(102, "anu");
+        map.put(103, "ram");
+        map.put(104, "ken");
         System.out.println(map);
-    }
+        map.remove(102);
+        System.out.println(map.containsKey(102));
+        System.out.println(map.containsValue("king"));
+        System.out.println(map.size());
+        System.out.println(map.keySet());
+        System.out.println(map.values());
+        System.out.println(map.entrySet());
+        HashMap<Integer, String> map2 = new HashMap<>();
+        map2.putAll(map);
+        System.out.println(map2);
+    }}
 
-}
+
+
