d862a236aa1c6df3a269f7f3f66e6ca4475bad7d
Parveen <parveen.mansuraligan@gmail.com>
2026-09-07 22:26:26 +0530

    predicate program

--- src/lambdaforeach.java
+++ src/lambdaforeach.java
@@ -1,14 +1,17 @@
 import java.util.*;
+import java.util.function.Predicate;
 public class lambdaforeach {
     public static void main(String[] args)
     {
         HashSet<Integer> set=new HashSet<>();
-        set.add(3);
-        set.add(8);
-        set.add(2);
+        set.add(30);
+        set.add(80);
+        set.add(20);
         System.out.println(set);
-        set.forEach(number->{if(number%2==0)
-        {System.out.println(number);
+        Predicate<Integer> p=number->number>20;
+        set.forEach(number->{if(p.test(number))
+        {
+            System.out.println(number);
         }});
     }
 }
