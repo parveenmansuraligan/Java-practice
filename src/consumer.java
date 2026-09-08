36ae25ad7c35cc873af1777474043546c19787be
Parveen <parveen.mansuraligan@gmail.com>
2026-09-08 19:26:48 +0530

    Consumer,Predicate  prgram

--- src/consumer.java
+++ src/consumer.java
@@ -1,14 +1,20 @@
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 public class consumer
 {
 public static void main(String args[])
 {
-    ArrayList<Integer> list=new ArrayList<>();
-    list.add(2);
-    list.add(89);
-    list.add(4);
-    System.out.println(list);
+    TreeSet<Integer> t=new TreeSet<>();
+    t.add(5);
+    t.add(1);
+    t.add(18);
+    t.add(21);
+    System.out.println(t);
+    Predicate<Integer> p=num->num>5;
     Consumer<Integer> c=num->System.out.println(num);
-    list.forEach(c);
-}}
+    t.forEach(num->{if(p.test(num)) {
+        c.accept(num);
+    }});
+    }
+}
