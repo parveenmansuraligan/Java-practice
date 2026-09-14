bca719c67c19b612476a40d0246fd1fc9cf97867
Parveen <parveen.mansuraligan@gmail.com>
2026-09-14 19:50:16 +0530

    Practice even or odd using interface

--- src/practicelambda1.java
+++ src/practicelambda1.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 public class practicelambda1
 {
     public static void main(String args[])
@@ -9,8 +10,16 @@ public class practicelambda1
         list.add(40);
         list.add(1);
         list.add(23);
-        Consumer<Integer> c=num->System.out.println(num);
-        list.forEach(c);
+        Predicate<Integer> p=num->(num%2==0);
+        Consumer<Integer> even=num->System.out.println(num +"is even");
+        Consumer<Integer> odd=num->System.out.println(num+"is odd");
+        list.forEach(num->{if(p.test(num))
+            {
+                even.accept(num);
+            }
+            else
+                odd.accept(num);});
+        }
 
-    }
-}
+        }
+
---
+++ src/practicelambda2.java
@@ -0,0 +1,2 @@
+public class practicelambda2 {
+}
