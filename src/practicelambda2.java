6f00729eabe58ef0b61fdfa94e3db89bc341ce81
Parveen <parveen.mansuraligan@gmail.com>
2026-09-14 20:19:41 +0530

    Practice program to uppercase using Function

--- src/practicelambda2.java
+++ src/practicelambda2.java
@@ -1,2 +1,16 @@
+import java.util.*;
+import java.util.function.Function;
 public class practicelambda2 {
+    public static void main(
+            String args[])
+    {
+     HashSet<String> set=new HashSet<>();
+     set.add("ken");
+     set.add("bob");
+     set.add("pom");
+     Function<String,String> f=name->name.toUpperCase();
+     for(String name:set){
+         String r=f.apply(name);
+     System.out.println(r);
+    }}
 }
