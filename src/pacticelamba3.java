98236901f98b0dcd44002561517a0859a97fc4bc
Parveen <parveen.mansuraligan@gmail.com>
2026-09-15 19:02:34 +0530

    Supplier program

--- src/pacticelamba3.java
+++ src/pacticelamba3.java
@@ -1,15 +1,12 @@
 import java.util.*;
-import java.util.function.Predicate;
+import java.util.function.Supplier;
 public class pacticelamba3 {
-    public static void main(String args[])
-    {
-        ArrayList<Integer> list=new ArrayList<>();
-        list.add(10);
-        list.add(3);
-        list.add(12);
-        Predicate<Integer> p=num->(num%2==0);
-        list.forEach(num->{if(p.test(num))
-        System.out.println(num+" " +"even");
-        });
-    }
-}
+    public static void main(String args[]) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("hai");
+        list.add("jello");
+        list.add("god");
+        Supplier<String> s = () -> "welcome to auto";
+        String r = s.get();
+        System.out.println(r);
+    }}
