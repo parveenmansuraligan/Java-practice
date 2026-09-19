7b8642eba54a05e3be6433d99291a7f0a99e2264
Parveen <parveen.mansuraligan@gmail.com>
2026-09-19 17:23:37 +0530

    common elements between list

--- src/arraylist3.java
+++ src/arraylist3.java
@@ -11,15 +11,14 @@ public class arraylist3
         list.add(100);
         list.add(34);
         System.out.println(list);
-        HashSet<Integer> set = new HashSet<>(list);
-        System.out.println(set);
-        for(int i=0;i<list.size();i++)
-        {
-            for(int j=i+1;j<list.size();j++)
-            {
-                if(list.get(i).equals(list.get(j)))
-                    System.out.println(list.get(i));
-
+        list1.add(34);
+        list1.add(78);
+        list1.add(100);
+        list1.add(20);
+        System.out.println(list1);
+        for (int num : list1) {
+            if (list.contains(num)) {
+                System.out.println(num);
             }
         }
     }}
