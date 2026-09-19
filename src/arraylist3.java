69321871ce7474fac6c3af97fbae0aa44bff67f8
Parveen <parveen.mansuraligan@gmail.com>
2026-09-19 15:55:47 +0530

    Reverse lsit

--- src/arraylist3.java
+++ src/arraylist3.java
@@ -1,25 +1,16 @@
 import java.util.*;
 public class arraylist3
 {
-    public static void main(String[] args)
-    {
-        ArrayList<Integer> list=new ArrayList<>();
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
         list.add(34);
         list.add(12);
         list.add(43);
         list.add(100);
-        int l=list.get(0);
-        int sl=list.get(0);
-        for(int num:list) {
-            if (num>l) {
-                sl=l;
-                l=num;
-            }
-            if (sl != l && sl > num) {
-                sl = num;
-            }}
-                System.out.println("largest" + l);
-                System.out.println("Second largest" + sl);
-        }
-    }
-
+        list.add(100);
+        list.add(34);
+        System.out.println(list);
+        HashSet<Integer> set = new HashSet<>(list);
+        System.out.println(set);
+    }}
