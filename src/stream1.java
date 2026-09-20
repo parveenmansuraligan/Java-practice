f44489e090092c073755489108bc07449658df42
Parveen <parveen.mansuraligan@gmail.com>
2026-09-20 09:50:24 +0530

    Stream2

--- src/stream1.java
+++ src/stream1.java
@@ -6,6 +6,9 @@ public class stream1 {
         list.add(20);
         list.add(1);
         list.add(34);
+        list.add(21);
+        list.add(12);
         list.stream().forEach(num->System.out.println(num));
-    }
-}
+        list.stream().filter(num->num%2==0).forEach(num->System.out.println("Even:" +num));
+    }}
+
