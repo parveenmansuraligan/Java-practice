2fd0b462383a1c9fa5c989dee2cc1aa48f5fd275
Parveen <parveen.mansuraligan@gmail.com>
2026-09-22 20:18:28 +0530

    Stream forEach program

---
+++ src/stream2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class stream2 {
+    public static void main(String args[])
+    {
+        List<Integer> number=new ArrayList<>();
+        number.add(10);
+        number.add(22);
+        number.add(20);
+        number.add(30);
+        number.stream().filter(n->n>20).forEach(n->System.out.println(n));
+    }
+}
